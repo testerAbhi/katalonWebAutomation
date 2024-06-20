f5d6b162923222b052419120327e23b1fc7afa07
testerAbhi <abhishek.kasera@devstringx.com>
2024-06-20 15:13:58 +0530

    removed

--- Test Suites/Web_Home - Copy.ts
+++ Test Suites/Web_Home - Copy.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Web_Home</name>
+   <name>Web_Home - Copy</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,7 +10,7 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>afb39da8-e0fc-44ce-a90f-41596b948c5a</testSuiteGuid>
+   <testSuiteGuid>9249c1b3-d3df-4d5f-b7af-30ca135f7501</testSuiteGuid>
    <testCaseLink>
       <guid>ba0c6f98-d38f-48ff-8361-d1b4531d98c5</guid>
       <isReuseDriver>false</isReuseDriver>
